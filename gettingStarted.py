88d0dd3b04aac8980912fed130f437e1ba34676a
Alexandra Miller <alexandramiller@Alexandras-MacBook-Pro.local>
2025-09-21 20:32:35 -0400

    update

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,31 +5,36 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    if question.casefold() == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?".casefold():
+        answer = "pcap"
+    elif question.casefold() == "Are encoding and encryption the same? - Yes/No".casefold():
+        answer = "No"
+    elif question.casefold() == "Is it possible to decrypt a message without a key? - Yes/No".casefold():
+        answer = "No"
+    elif question.casefold() == "is it possible to decode a message without a key? - yes/no".casefold():
+        answer = "Yes"
+    elif question.casefold() == "Is a hashed message supposed to be un-hashed? - Yes/No".casefold():
+        answer = "No"
+    elif question.casefold() == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ".casefold():
+        answer = "76b20c54e4ea5ac0c549e87900c856cf6b4d7ee828fab10a6e82642dedae320e"
+    elif question.casefold() == "Is MD5 a secured hashing algorithm? - Yes/No".casefold():
+        answer = "No"
+    elif question.casefold() == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number".casefold():
+        answer = 5
+    elif question.casefold() == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number".casefold():
+        answer = 3
+    ### you should understand why this else case should be included
+    ### what happens if there is a typo in one of the questions?
+    ### maybe put something here to flag an issue and catch errors
+    else:
+        raise Exception("This is not a valid question, maybe there is a typo")
     return(answer)
 # Complete all the questions.
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = "are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
-#Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
